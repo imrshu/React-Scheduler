9d29ef712ca609e36d5b5528efcd388f303a30d6
imrshu <rishabh.verma11998@gmail.com>
2020-07-26 20:30:59 +0530

    Ui: change apperance modal popup

--- src/App.js
+++ src/App.js
@@ -1,26 +1,15 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 import { ScheduleComponent, Inject, Week, Day, Month, DragAndDrop } from '@syncfusion/ej2-react-schedule';
 
 
 class App extends Component {
-
-  data = {
-    dataSource: [
-      {
-        Id: 1,
-        StartTime: new Date(2020, 7, 26),
-        EndTime: new Date(2020, 7, 26),
-        Subject: 'Justin Clark'
-      }
-    ]
-  }
-
   render() {
     return (
-      <ScheduleComponent eventSettings={this.data} timezone='Asia/Kolkata'>
+      <ScheduleComponent
+       quickInfoTemplates={{header: '#headerTemplate', content: '#contentTemplate', footer: '#footerTemplate'}}
+       timezone='Asia/Kolkata'>
         <Inject services={[Week, Day, Month, DragAndDrop ]} />
       </ScheduleComponent>
     );
